9c188d1eaea5e90d19bc93ca4fa3a01de9f59c38
Yasutaka Iwami <yas.iwami@gmail.com>
2019-11-18 07:16:43 +0900

    solved a in 141

---
+++ src/main/java/141/a.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    val w = readLine()!!
+    when (w) {
+        "Sunny" -> println("Cloudy")
+        "Cloudy" -> println("Rainy")
+        else -> println("Sunny")
+    }
+}
