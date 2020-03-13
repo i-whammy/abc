60dc615ce5385f8e07b020448b3cee2a1e5649a2
i-whammy <yas.iwami@gmail.com>
2020-03-13 23:35:21 +0900

    mod 158-b

--- src/main/kotlin/158/b.kt
+++ src/main/kotlin/158/b.kt
@@ -1,6 +1,6 @@
 import java.lang.Math
 
 fun main(args: Array<String>) {
-    val (n,a,b) = readLine()!!.split(" ").map { it.toInt() }
+    val (n,a,b) = readLine()!!.split(" ").map { it.toLong() }
     println(n / (a + b) * a + Math.min(a, n % (a + b)))
 }
