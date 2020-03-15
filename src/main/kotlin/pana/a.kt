3900fd9fc9bd499ab95e928a0a72b15d7d080356
i-whammy <yas.iwami@gmail.com>
2020-03-15 23:04:51 +0900

    solved pana a

---
+++ src/main/kotlin/pana/a.kt
@@ -0,0 +1,4 @@
+fun main(args: Array<String>) {
+    val strings = "1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51"
+    println(strings.split(", ")[readLine()!!.toInt()-1])
+}
